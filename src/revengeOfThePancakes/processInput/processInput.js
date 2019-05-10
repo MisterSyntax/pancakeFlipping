e1ce9eba822b18704c97fe5b0d6e7af9613fc330
Andrew Syriala <asyriala@overstock.com>
2019-05-10 16:56:17 -0600

    Add base functionality

---
+++ src/revengeOfThePancakes/processInput/processInput.js
@@ -0,0 +1,11 @@
+export default function processInput(inputString = '') {
+    const splitInput = inputString.trim().split(/\n/);
+    if (splitInput.length < 2) {
+        return ["0",[]];
+    }
+
+    const numberOfStrings = splitInput[0];
+    const arrayOfArrays = splitInput.slice(1).map(curr => curr.trim().split(''));
+
+    return [numberOfStrings, arrayOfArrays];
+}
---
+++ src/revengeOfThePancakes/processInput/processInput.test.js
@@ -0,0 +1,23 @@
+import processInput from './processInput';
+
+describe('parses a giant input string and returns an array with the number of stacks and an array of stacks', () => {
+    it('handles 1 test case', () => {
+        expect(processInput(
+            `1
+            -
+            `)
+        ).toEqual(["1", [['-']]]);
+    });
+    it('handles multiple test cases', () => {
+        expect(processInput(
+            `2
+            -
+            ++
+            `)
+        ).toEqual(["2", [['-'], ['+','+']]]);
+    });
+    it('handles bad input', () => {
+        expect(processInput()).toEqual(["0", []]);
+        expect(processInput('1++++++++')).toEqual(["0", []]);
+    });
+});
