f9a3ebc1487cf408c7766dc46a291f8783ae8f84
Andrew Syriala <asyriala@overstock.com>
2019-05-10 19:05:03 -0600

    Added in a comment as how to run tests

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import revengeOfThePancakes from './revengeOfThePancakes/revengeOfThePancakes.js';
+
+/**
+* Add any tests below in the following format
+* revengeOfThePancakes takes a string as input and returns a string as output
+* you'll need to log it if you want to see the result
+**/
+console.log(revengeOfThePancakes(`
+    5
+    -
+    -+
+    +­-+
+    +++
+    --+-
+`));
